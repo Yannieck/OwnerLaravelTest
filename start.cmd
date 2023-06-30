edf3c1bed2da6759cdd4f036645be519b942b0c4
Yannieck Blaauw <91061289+Yannieck@users.noreply.github.com>
2023-06-30 22:36:06 +0200

    Create start.cmd

---
+++ start.cmd
@@ -0,0 +1,11 @@
+npm install
+
+composer install
+
+php artisan key:generate
+
+php artisan migrate --path=./database/migrations/2019_08_19_000000_create_failed_jobs_table.php
+php artisan migrate --path=./database/migrations/2014_10_12_000000_create_users_table.php
+php artisan migrate --path=./database/migrations/2020_03_09_051125_create_products_table.php
+
+php artisan serve
